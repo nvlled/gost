80cbf7745482744f359ac035b23da01fc942754e
ron <nvlled@gmail.com>
2014-12-12 14:49:45 +0800

    Re-organize main package

---
+++ common.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nvlled/gost/defaults"
+	"github.com/nvlled/gost/genv"
+	"github.com/nvlled/gost/util"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"text/template"
+)
+
+const (
+	// distdel: directory is safe to delete
+	MARKER_NAME       = ".gost-distdel"
+	VERBATIM_KEY      = "verbatim"
+	INCLUDES_DIR_KEY  = "includes-dir"
+	LAYOUTS_DIR_KEY   = "layouts-dir"
+	TEMPLATES_DIR_KEY = "templates-dir"
+)
+
+type Index map[string]genv.T
+
+var index Index
+var pathIndex Index
+
+var includesDir string
+var layoutsDir string
+var templatesDir string
+
+var baseEnv = genv.T{
+	INCLUDES_DIR_KEY:  defaults.INCLUDES_DIR,
+	LAYOUTS_DIR_KEY:   defaults.LAYOUTS_DIR,
+	TEMPLATES_DIR_KEY: defaults.TEMPLATES_DIR,
+}
+
+var globalFuncMap = template.FuncMap{
+	"genid": generateId,
+	"shell": util.Exec,
+}
+
+func printLog(args ...interface{}) {
+	if verbose {
+		fmt.Println(args...)
+	}
+}
+
+func isValidBuildDir(dir string) bool {
+	if _, err := os.Lstat(dir); err != nil {
+		if os.IsNotExist(err) {
+			return true
+		}
+	}
+	_, err := os.Open(join(dir, MARKER_NAME))
+	return err == nil
+}
+
+func subDirList(baseDir string, path string) []string {
+	sep := string(filepath.Separator)
+	dirs := strings.Split(path, sep)
+
+	result := []string{baseDir}
+	for _, dir := range dirs {
+		result = append(result, join(baseDir, dir))
+	}
+	return result
+}
+
+func writeMarker(dir string) {
+	_, err := os.Create(join(dir, MARKER_NAME))
+	fail(err)
+}
+
+func generateId() string {
+	return util.RandomString()[:5]
+}
+
+func join(path ...string) string {
+	return filepath.Join(path...)
+}
+
+func createTemplate() *template.Template {
+	funcMap := createFuncMap(".")
+	for k, v := range globalFuncMap {
+		funcMap[k] = v
+	}
+
+	return template.New("default").Funcs(funcMap)
+}
+
+func fail(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func isVerbatim(env genv.T, path string) bool {
+	if env != nil {
+		for _, pref := range verbatimList {
+			if strings.HasPrefix(path, pref) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func globTemplates(t *template.Template, key, dir string) {
+	if !util.DirExists(dir) {
+		println("**", key, dir, "not found")
+	} else if !util.IsDirEmpty(dir) {
+		_, err := t.ParseGlob(join(dir, "*.html"))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func errHandler() {
+	err := recover()
+	if err != nil {
+		fmt.Printf("*** error %v\n", err)
+	}
+}
+
+func isItemplate(path string) bool {
+	ext := filepath.Ext(path)
+	return ext == ".html" ||
+		ext == ".js" ||
+		ext == ".css"
+
+}
+
+func skipFile(file string) bool {
+	base := filepath.Base(file)
+	dir := filepath.Dir(file)
+	return strings.HasPrefix(base, ".") || //exclude dot files
+		base == genv.FILENAME ||
+		dir == includesDir ||
+		dir == layoutsDir ||
+		dir == templatesDir ||
+		base == MARKER_NAME
+}
+
+func genUrl(srcPath, destPath string) string {
+	re := regexp.MustCompile(`^/`)
+	if re.MatchString(destPath) {
+		return destPath
+	}
+	if re.MatchString(srcPath) {
+		return join(srcPath, destPath)
+	}
+
+	sep := string(filepath.Separator)
+	prefix := util.CommonSubPath(destPath, srcPath) + sep
+
+	srcPath_ := strings.TrimPrefix(srcPath, prefix)
+	destPath_ := strings.TrimPrefix(destPath, prefix)
+
+	dlevel := util.DirLevel(destPath_)
+	slevel := util.DirLevel(srcPath_)
+
+	if slevel >= dlevel {
+		paths := util.Times("..", slevel-1)
+		paths = append(paths, destPath_)
+		return join(paths...)
+	} else {
+		return destPath
+	}
+}
+
+func createFuncMap(curPath string) template.FuncMap {
+	return template.FuncMap{
+		"url": func(path string) string {
+			return genUrl(curPath, path)
+		},
+		"urlfor": func(id string) string {
+			if env, ok := index[id]; ok {
+				return genUrl(curPath, env.Get("path"))
+			}
+			return "#nope"
+		},
+		"with_env": func(key string, value interface{}) []interface{} {
+			var envs []interface{}
+			for _, env := range index {
+				v := env.Get(key)
+				if value == v {
+					envs = append(envs, env)
+				}
+			}
+			return envs
+		},
+	}
+}
---
+++ gost.go
@@ -0,0 +1,282 @@
+package main
+
+import (
+	"bytes"
+	"github.com/nvlled/gost/genv"
+	"github.com/nvlled/gost/util"
+	"gopkg.in/fsnotify.v1"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
+
+var actions = map[string]func(args []string){
+	"build": func(_ []string) {
+		runBuild()
+	},
+	"watch": func(_ []string) {
+		runBuild()
+		printLog("watching", srcDir)
+		watcher, err := fsnotify.NewWatcher()
+		util.RecursiveWatch(watcher, srcDir)
+		fail(err)
+		rebuild := util.Throttle(runBuild, 900)
+		for {
+			select {
+			case e := <-watcher.Events:
+				printLog(">", e.String())
+				rebuild()
+			}
+		}
+	},
+	"clean": func(_ []string) {
+		cleanBuildDir(srcDir, destDir)
+	},
+	"make": func(args []string) {
+		defer errHandler()
+		if len(args) == 0 {
+			println("missings args: make <path> [title]")
+			println("Note: path must be relative to source directory:", srcDir)
+			return
+		}
+		path := args[0]
+		var title string
+		if len(args) > 1 {
+			title = args[1]
+		}
+		makeFile(path, title)
+	},
+}
+
+func runBuild() {
+	defer errHandler()
+
+	index = make(Index)
+	pathIndex = make(Index)
+	env := genv.ReadDir(srcDir)
+	baseEnv = genv.Merge(env, baseEnv)
+
+	initializeDirs(baseEnv)
+	verbatimList = strings.Fields(baseEnv.Get(VERBATIM_KEY))
+
+	printLog("building index...")
+	buildIndex(srcDir, baseEnv)
+
+	t := createTemplate()
+	printLog("loading includes", includesDir)
+	loadIncludes(t, includesDir)
+	printLog("loading layouts", layoutsDir)
+	loadLayouts(t, layoutsDir)
+	printLog("building output...", layoutsDir)
+	buildOutput(t, srcDir, destDir)
+	println("** done.")
+}
+
+func makeFile(path, title string) {
+	fullpath := join(srcDir, path)
+	fulldir := filepath.Dir(fullpath)
+
+	if info, err := os.Lstat(fullpath); err == nil {
+		if info.IsDir() {
+			println("file is a directory:", fullpath)
+		} else {
+			println("file already exists:", fullpath)
+		}
+		return
+	}
+
+	if _, err := os.Lstat(fulldir); os.IsNotExist(err) {
+		println("directory does not exist:", fulldir)
+		return
+	}
+
+	env := make(genv.T)
+	for _, dir := range subDirList(srcDir, filepath.Dir(path)) {
+		parentEnv := genv.ReadDir(dir)
+		env = genv.Merge(env, parentEnv)
+	}
+	if title != "" {
+		env["title"] = title
+	}
+
+	templName := env.Get("template")
+	templDir := join(srcDir, env.Get("templates-dir"))
+
+	if templName == "" {
+		println("no template for file", fullpath)
+		println("add `template: the-template-name` in env")
+		return
+	}
+
+	t := createTemplate()
+	t.Delims("[[", "]]")
+	loadMakeTemplates(t, templDir)
+
+	file, err := os.Create(fullpath)
+	fail(err)
+	t = t.Lookup(templName)
+	if t == nil {
+		println("template not found:", templName)
+		return
+	}
+	printLog("using", "`"+templName+"`", "template from", templDir)
+	err = t.ExecuteTemplate(file, templName, env)
+	printLog("file created ->", fullpath)
+	fail(err)
+}
+
+func cleanBuildDir(srcDir, destDir string) {
+	if isValidBuildDir(destDir) {
+		printLog("cleaning", destDir)
+		os.RemoveAll(destDir)
+		return
+	}
+
+	dirs, err := util.ReadDir(destDir, func(path string) bool {
+		path = join(srcDir, strings.TrimPrefix(path, destDir))
+		if _, err := os.Lstat(path); err == nil {
+			return false
+		}
+		return true
+	})
+	if err != nil {
+		panic(err)
+	}
+	for _, dir := range dirs {
+		dir = join(destDir, dir)
+		printLog("removing", dir)
+		os.RemoveAll(dir)
+	}
+}
+
+func buildIndex(path string, parentEnv genv.T) {
+	info, err := os.Lstat(path)
+	if err != nil {
+		log.Println(err)
+	} else if info.IsDir() {
+		// FIX: baseEnv is read twice
+		env := genv.ReadDir(path)
+		env = genv.Merge(env, parentEnv)
+
+		dirs, err := util.ReadDir(path, skipFile)
+
+		if err != nil {
+			log.Println(err)
+		} else {
+			for _, name := range dirs {
+				subpath := join(path, name)
+				buildIndex(subpath, env)
+			}
+		}
+	} else if isItemplate(path) {
+		env := genv.ReadEnv(path)
+		env = genv.Merge(env, parentEnv)
+		//env["path"] = strings.TrimPrefix(path, srcDir)
+		env["path"] = join("/", strings.TrimPrefix(path, srcDir))
+
+		pathIndex[path] = env
+		if id, ok := env.GetOk("id"); ok {
+			if otherEnv, dokie := index[id]; dokie {
+				otherPath := otherEnv["path"]
+				log.Println("Duplicate id for paths", path, otherPath)
+			}
+			printLog("adding", path, "to index, id =", id)
+			index[id] = env
+		} else {
+			printLog("omitting", path, "from index (no id)")
+		}
+	}
+}
+
+func buildOutput(t *template.Template, srcDir, destDir string) {
+	if isValidBuildDir(destDir) {
+		printLog("cleaning", destDir)
+		os.RemoveAll(destDir)
+		util.Mkdir(destDir)
+		writeMarker(destDir)
+	}
+
+	fn := func(srcPath string, info os.FileInfo, _ error) (err error) {
+		if skipFile(srcPath) || info.IsDir() {
+			return
+		}
+
+		s := strings.TrimPrefix(srcPath, srcDir)
+		destPath := join(destDir, s)
+		util.Mkdir(filepath.Dir(destPath))
+
+		env := pathIndex[srcPath]
+		if isItemplate(srcPath) && !isVerbatim(env, s) {
+			s := genv.ReadContents(srcPath)
+			s = applyTemplate(t, s, env)
+
+			if filepath.Ext(srcPath) == ".html" {
+				s = applyLayout(t, s, env)
+			}
+
+			printLog("rendering", srcPath, "->", destPath)
+			err = ioutil.WriteFile(destPath, []byte(s), 0644)
+		} else {
+			printLog("copying", srcPath, "->", destPath)
+			err = util.CopyFile(destPath, srcPath)
+		}
+		return
+	}
+	filepath.Walk(srcDir, fn)
+}
+
+func initializeDirs(env genv.T) {
+	prependSrc := func(dir string) string {
+		if dir != "" {
+			dir = path.Clean(join(srcDir, dir))
+		}
+		return dir
+	}
+	includesDir = prependSrc(env.Get(INCLUDES_DIR_KEY))
+	layoutsDir = prependSrc(env.Get(LAYOUTS_DIR_KEY))
+	templatesDir = prependSrc(env.Get(TEMPLATES_DIR_KEY))
+}
+
+func applyTemplate(t *template.Template, s string, env genv.T) string {
+	curPath := env.Get("path")
+	buf := new(bytes.Buffer)
+	funcs := createFuncMap(curPath)
+	err := template.Must(t.New(curPath).Funcs(funcs).Parse(s)).Execute(buf, env)
+	fail(err)
+	return buf.String()
+}
+
+func applyLayout(t *template.Template, s string, env genv.T) string {
+	layout := env.Get("layout")
+	if layout == "" {
+		return s
+	}
+
+	env["Contents"] = s
+	env["contents"] = s
+	env["Body"] = s
+	env["body"] = s
+
+	curPath := env.Get("path")
+	buf := new(bytes.Buffer)
+	funcs := createFuncMap(curPath)
+	err := t.New(curPath).Funcs(funcs).ExecuteTemplate(buf, layout, env)
+	fail(err)
+	return buf.String()
+}
+
+func loadIncludes(t *template.Template, dir string) {
+	globTemplates(t, "includes-dir", dir)
+}
+
+func loadLayouts(t *template.Template, dir string) {
+	globTemplates(t, "layouts-dir", dir)
+}
+
+func loadMakeTemplates(t *template.Template, dir string) {
+	globTemplates(t, "templates-dir", dir)
+}
